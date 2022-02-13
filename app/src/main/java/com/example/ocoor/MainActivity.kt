4c00fc17b36a804dc65b5284f30c7cf65d8df9f9
Crackj675 <paulrohde@hotmail.de>
2022-02-13 18:08:24 +0100

    finished YT guide (not running)

--- app/src/main/java/com/example/ocoor/MainActivity.kt
+++ app/src/main/java/com/example/ocoor/MainActivity.kt
@@ -1,25 +1,43 @@
 package com.example.ocoor
 
-import com.theartofdev.edmodo
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.util.SparseArray
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.ocoor.ui.main.MainFragment
 import com.example.ocoor.databinding.MainActivityBinding
 import com.fenchtose.nocropper.Cropper
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.TextRecognition
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var butten_capture:Button;
-    lateinit var butten_copy:Button;
+    lateinit var button_capture:Button;
+    lateinit var button_copy:Button;
+    lateinit var message:TextView;
     private lateinit var binding: MainActivityBinding
+    lateinit var bitmap: Bitmap
 
     val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
@@ -39,16 +57,59 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        butten_capture = binding.buttonCapture
-        butten_copy = binding.buttonCopy
+        button_capture = binding.buttonCapture
+        button_copy = binding.buttonCopy
+        message = binding.message
 
         if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
         }
 
-        butten_capture.setOnClickListener{
-            Cropper
+        button_capture.setOnClickListener{
+            CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(this)
         }
+
+        button_copy.setOnClickListener {
+            var scanned_text:String = message.text.toString()
+            copyToClipBoard(scanned_text)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            var result:CropImage.ActivityResult = CropImage.getActivityResult(data)
+            if(resultCode == RESULT_OK) run {
+                var resultUri: Uri = result.getUri()
+                bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, resultUri)
+                getTextFromImage(bitmap)
+            }
+        }
+    }
+    private fun getTextFromImage(bitmap:Bitmap){
+        val recognizer:TextRecognizer = TextRecognizer.Builder(this).build()
+        if(!recognizer.isOperational){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT)
+        }else {
+            val frame:Frame = Frame.Builder().setBitmap(bitmap).build()
+            val textBlockSparseArray: SparseArray<TextBlock> = recognizer.detect(frame)
+            val stringBuilder:StringBuilder = StringBuilder()
+            for (i in 0..textBlockSparseArray.size()){
+                val textBlock:TextBlock = textBlockSparseArray.valueAt(i)
+                stringBuilder.append(textBlock.value)
+                stringBuilder.append("\n")
+            }
+            binding.message.setText(stringBuilder.toString())
+            button_capture.setText("Retake")
+            button_copy.visibility = View.VISIBLE
+        }
+    }
+
+    private fun copyToClipBoard(text:String){
+        val clipboard:ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip:ClipData = ClipData.newPlainText("Copied data", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, "Copied to Clipboard", Toast.LENGTH_SHORT).show()
     }
 }
 
