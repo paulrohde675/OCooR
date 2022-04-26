f9df1dfded0e44dd672ba920dc882bf4e0634f55
Paul <Paulrohde@hotmail.de>
2022-04-26 22:03:14 +0200

    Add: started movable slider

--- app/src/main/java/com/example/listed/Fragments/ActiveRecylerViewFragment.kt
+++ app/src/main/java/com/example/listed/Fragments/ActiveRecylerViewFragment.kt
@@ -1,10 +1,8 @@
 package com.example.ocoor.Fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ocoor.Adapter.InactiveItemAdapter
@@ -12,7 +10,6 @@ import com.example.ocoor.Adapter.ItemAdapter
 import com.example.ocoor.MainActivity
 import com.example.ocoor.Utils.Item
 import com.example.ocoor.databinding.ActiveRecyclerViewFragmentBinding
-import java.util.Observer
 
 class ActiveRecyclerViewFragment : Fragment() {
 
@@ -45,6 +42,94 @@ class ActiveRecyclerViewFragment : Fragment() {
         inactiveItemRecyclerView.layoutManager = LinearLayoutManager(context)
         inactiveItemRecyclerView.adapter = inactiveItemAdapter
 
+        /*
+        binding.slider.setOnDragListener { view, dragEvent ->
+
+            //2
+            val draggableItem = dragEvent.localState as View
+
+            println("Drag")
+
+            //3
+            when (dragEvent.action) {
+                DragEvent.ACTION_DRAG_STARTED -> {
+                    true
+                }
+                DragEvent.ACTION_DRAG_ENTERED -> {
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DRAG_LOCATION -> {
+                    true
+                }
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    true
+                }
+                DragEvent.ACTION_DROP -> {
+                    true
+                }
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+        */
+
+        var dX = 0f
+        var dY = 0f
+
+        binding.slider.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+
+                when (event!!.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        // apply the change to the dimensions of your view;
+                        // for example with animation and using scale parameters, like this:
+                        v!!.animate().scaleX(0.6f).scaleY(0.6f)
+
+                        dX = v.getX() - event.getRawX();
+                        dY = v.getY() - event.getRawY();
+
+                        return true
+                    }
+                    MotionEvent.ACTION_CANCEL,
+
+                    MotionEvent.ACTION_UP ->                 // revert to the normal dimensions of your view
+                        // for example with animation and using scale parameters, like this:
+
+                    {
+                        v!!.animate().scaleX(1f).scaleY(1f)
+                        v!!.animate().x(event.getRawX() + dX).x(event.getRawX() + dX).y(event.getRawY() + dY).setDuration(0).start()
+
+                    }
+
+
+
+
+                }
+                return false
+            }
+        })
+
+
+        /*
+        myOnTouchListener = new OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent me){
+                if (me.getAction() == MotionEvent.ACTION_DOWN){
+                    oldXvalue = me.getX();
+                    oldYvalue = me.getY();
+                    Log.i(myTag, "Action Down " + oldXvalue + "," + oldYvalue);
+                }else if (me.getAction() == MotionEvent.ACTION_MOVE  ){
+                    LayoutParams params = new LayoutParams(v.getWidth(), v.getHeight(),(int)(me.getRawX() - (v.getWidth() / 2)), (int)(me.getRawY() - (v.getHeight())));
+                    v.setLayoutParams(params);
+                }
+                return true;
+            }
+         */
         return binding.root
     }
 
